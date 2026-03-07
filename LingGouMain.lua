b76382d258c56128e4998658f360e3d45fdd3c10
L_Dxezd <3164396703@qq.com>
2026-03-07 19:53:10 +0800

    Delete LingGouMain.lua

--- LingGouMain.lua
+++
@@ -1,6 +0,0 @@
---Make By AC
---Version 1.7.0
---Fix some bugs fr now
---use script now
---Time 2026/1/23
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/fbcca215fc0c343b78f0d207ed71faaf6d0fb33a5d3c4c56ebbec80f17dc6d54/download"))()
